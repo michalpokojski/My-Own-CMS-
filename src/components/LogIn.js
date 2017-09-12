c953a099359adb92f87bc78f417d35e567dd8e4b
Michał Pokojski <michal.pokojski@gmail.com>
2017-09-12 16:52:09 +0200

    removed old code

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -7,16 +7,6 @@ import { MuiThemeProvider } from 'material-ui/styles'
 
 const required = value => (value ? undefined : 'Required')
 
-const renderField = ({input, label, type, meta: {touched, error, warning}}) => (
-  <div>
-    <label>{label}</label>
-    <div>
-      <input {...input} placeholder={label} type={type}/>
-      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
-    </div>
-  </div>
-)
-
 export const renderTextField = ({input, label, meta: {touched, error}, ...custom},) => (
   <MuiThemeProvider>
     <TextField

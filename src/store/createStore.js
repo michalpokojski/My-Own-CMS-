4a12ff05d377530a978411ad31a59b9556ebd5df
Michał Pokojski <michal.pokojski@gmail.com>
2017-09-13 11:51:49 +0200

    Fixed bug with no localstorage provided

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -12,6 +12,6 @@ export default initialState => {
       applyMiddleware(
         thunk
       ),
-      persistState(['user'], {key: 'users'})
+      persistState(['auth'], {key: 'userLoggedIn'})
     ))
 }

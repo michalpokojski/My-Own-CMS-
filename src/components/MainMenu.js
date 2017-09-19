f209b4097f607074e9f257fefc885af8703eadc4
Michał Pokojski <michal.pokojski@gmail.com>
2017-09-19 09:00:57 +0200

    Main menu working, and log out button too

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {Tabs, Tab} from 'material-ui/Tabs';
+import { Tabs, Tab } from 'material-ui/Tabs';
+import Posts from './Posts'
+import Users from './Users'
 
 const MainMenu = (props) => (
   <Tabs>
-    <Tab label="Posts" >
-      <div>
-      </div>
+    <Tab label="Posts">
+      <Posts />
     </Tab>
-    <Tab label="Users" >
-      <div>
-      </div>
+    <Tab label="Users">
+      <Users />
     </Tab>
-    <div style={{width: '25%'}} />
-    <Tab onActive={ () => props.logOutUser()} label="Logout" />
+    <div style={{width: '25%'}}/>
+    <Tab onActive={() => props.logOutUser()} label="Logout"/>
 
   </Tabs>
 );
